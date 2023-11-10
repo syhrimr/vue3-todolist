c0db278ed7d5cfc6d541c08147fc260a51a4e3c7
Syahri <syahri.maulana@gmail.com>
2023-11-10 17:58:34 +0700

    feat: add styling to todo form

--- src/components/ToDoInput.vue
+++ src/components/ToDoInput.vue
@@ -1,14 +1,24 @@
 <template>
-  <form @submit.prevent="addToDoItem">
-    <input
-      v-model="title"
-      placeholder="Add your new to do item"
-    />
-    <input
-      v-model="description"
-      placeholder="(Optional) Add detail to your to do item"
-    />
-    <button type="submit" title="Add">Add</button>
+  <form class="todo-form" @submit.prevent="addToDoItem">
+    <label>
+      <span class="required">Title</span>
+      <input
+        v-model="title"
+        type="text"
+        placeholder="Add your new to do item"
+      />
+    </label>
+
+    <label>
+      <span>Description</span>
+      <input
+        v-model="description"
+        type="text"
+        placeholder="(Optional) Add detail to your to do item"
+      />
+    </label>
+
+    <button type="submit" title="Add new Todo item">Add</button>
   </form>
 </template>
 
@@ -31,3 +41,47 @@ function addToDoItem(): void {
   description.value = "";
 }
 </script>
+
+<style scoped lang="scss">
+.todo-form {
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  label {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    gap: 0.5rem;
+
+    span.required {
+      &::after {
+        content: " *";
+        color: red;
+      }
+    }
+  }
+
+  input {
+    height: 1.5rem;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    border-width: 1px;
+    outline: 1px;
+    margin-bottom: 0.75rem;
+  }
+
+  button {
+    cursor: pointer;
+    height: 2rem;
+    border-radius: 0.5rem;
+    border-width: 0px;
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+
+    &:hover {
+      transform: translateY(-2px);
+    }
+  }
+}
+</style>
