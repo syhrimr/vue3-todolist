931403ff33d9a14d91d9265fa48fd899436e207b
Syahri <syahri.maulana@gmail.com>
2023-11-10 13:19:48 +0700

    feat: add to do listing

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ToDoInput @add="receiveInput" />
+  <ul>
+    <template v-for="item in list" :key="item">
+      <li>
+        <ToDoItem :todo="item" />
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script setup>
+import ToDoInput from './ToDoInput.vue';
+import ToDoItem from './ToDoItem.vue';
+
+import { ref, onMounted } from 'vue';
+
+const list = ref([]);
+
+onMounted(() => {
+  list.value = [
+    {
+      title: "Lunch",
+      description: "Lunch with banana",
+      isDone: false
+    }
+  ]
+});
+
+function receiveInput(value) {
+  list.value.unshift(value);
+}
+</script>
