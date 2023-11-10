0caa22566fd25d773c3ed9af0eb6ef563bb658a6
Syahri <syahri.maulana@gmail.com>
2023-11-10 13:19:29 +0700

    feat: add to do item to store an item data

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="item_text-wrapper">
+      <h1>{{ props.todo.title }}</h1>
+      <p>{{ props.todo.description }}</p>
+    </div>
+
+    <div class="item_buttons">
+      <button>Done</button>
+      <button>Delete</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  todo: {
+    type: Object,
+    required: true,
+    default: () => {}
+  }
+})
+</script>
