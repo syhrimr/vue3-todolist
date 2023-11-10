7eabbf09a697c57cae72df9b8ef9402cc72db5c2
Syahri <syahri.maulana@gmail.com>
2023-11-10 11:20:25 +0700

    feat: create an input form to add to do item

---
+++ src/components/ToDoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="addToDoItem">
+    <input v-model="todo" placeholder="Add your new to do item" />
+    <input v-model="description" placeholder="Add detail to your to do item" />
+    <button type="submit" title="Add">Add</button>
+  </form>
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const emits = defineEmits(["add"])
+
+const todo = ref("");
+const description = ref("");
+
+function addToDoItem() {
+  emits("add", {
+    todo: todo.value,
+    description: description.value,
+    status: "onprogress"
+  });
+
+  todo.value = "";
+  description.value = "";
+}
+</script>
