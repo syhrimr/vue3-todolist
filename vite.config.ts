b1739ffecf21e19dfb90135596d739f9a5ce78ec
Syahri <syahri.maulana@gmail.com>
2023-11-21 17:39:50 +0700

    chore: install and setup vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import * as path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: "happy-dom",
+    globals: true
+  },
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
